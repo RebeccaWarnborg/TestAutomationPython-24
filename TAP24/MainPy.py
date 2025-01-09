d7d05a6196cc4fcdcf11adc071242bcdd31efeeb
RebeccaWarnborg <rebecca.warnborg@hotmail.com>
2025-01-09 11:41:38 +0100

    Lagt till mer kod

--- TAP24/MainPy.py
+++ TAP24/MainPy.py
@@ -1,9 +1,24 @@
 # print('Hello world')
 # print('This program was made by Rebecca')
 
-Biljett = 100  # biljettpris
-Pengar = 200  # pengar på fickan
-Summa = Pengar - Biljett
-print("Det blir ", (Summa), "kronor över.")
-Hälften = 200 - 100 / 2
-print("Hälften är:", f"{Hälften:.0f}", "kr.")  # Visar utan decimaler
+#Biljett = 100  # biljettpris.
+#Pengar = 200  # pengar på fickan.
+#Summa = Pengar - Biljett
+#print("Det blir ", (Summa), "kronor över.")
+#Hälften = 200 - 100 / 2
+#print("Hälften är:", f"{Hälften:.0f}", "kr.")  # Visar utan decimaler.
+
+#tal1 = int(input("Skriv ett heltal ")) # Första heltalet från användaren.
+#tal2 = int(input("Skriv ett till heltal ")) # Andra heltalet från användaren. 
+#summan = tal1 + tal2 # Adderar talen.
+#print(summan)
+
+#Jacka = 2000 # Jackans pris.
+#Rea = 1000 # Rean på jackan. 
+#Betala = Jacka - Rea # Vad man ska betala.
+#print(Betala,"kr.") 
+
+Pris = 2000 # Jackans pris.
+Rea_procent = 50 # Rean på jackan.
+slut_pris = Pris * Rea_procent / 100 # Vad man ska betala.
+print(slut_pris) 
