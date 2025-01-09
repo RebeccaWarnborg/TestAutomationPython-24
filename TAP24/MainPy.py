ecb77ba70c9890b704738b1b626ed84f67478f68
RebeccaWarnborg <rebecca.warnborg@hotmail.com>
2025-01-09 10:41:47 +0100

    Lagt till kod

--- TAP24/MainPy.py
+++ TAP24/MainPy.py
@@ -1,2 +1,9 @@
-print('Hello world')
-print('This program was made by Rebecca')
+# print('Hello world')
+# print('This program was made by Rebecca')
+
+Biljett = 100  # biljettpris
+Pengar = 200  # pengar på fickan
+Summa = Pengar - Biljett
+print("Det blir ", (Summa), "kronor över.")
+Hälften = 200 - 100 / 2
+print("Hälften är:", f"{Hälften:.0f}", "kr.")  # Visar utan decimaler
