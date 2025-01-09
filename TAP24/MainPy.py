f4e424c6e82015bf78770a276de86d4cf3294546
RebeccaWarnborg <rebecca.warnborg@hotmail.com>
2025-01-09 12:04:14 +0100

    Gjort ändringar och lagt till kod

--- TAP24/MainPy.py
+++ TAP24/MainPy.py
@@ -1,27 +1,29 @@
-# print('Hello world')
-# print('This program was made by Rebecca')
+ print('Hello world')
+ print('This program was made by Rebecca')
 
-#Biljett = 100  # biljettpris.
-#Pengar = 200  # pengar på fickan.
-#Summa = Pengar - Biljett
-#print("Det blir ", (Summa), "kronor över.")
-#Hälften = 200 - 100 / 2
-#print("Hälften är:", f"{Hälften:.0f}", "kr.")  # Visar utan decimaler.
+biljett = 100  # biljettpris.
+pengar = 200  # pengar på fickan.
+summa = pengar - biljett
+print("Det blir ", (summa), "kronor över.")
+hälften = 200 - 100 / 2
+print("Hälften är:", f"{hälften:.0f}", "kr.")  # Visar utan decimaler.
 
-#tal1 = int(input("Skriv ett heltal ")) # Första heltalet från användaren.
-#tal2 = int(input("Skriv ett till heltal ")) # Andra heltalet från användaren. 
-#summan = tal1 + tal2 # Adderar talen.
-#print(summan)
+tal1 = int(input("Skriv ett heltal ")) # Första heltalet från användaren.
+tal2 = int(input("Skriv ett till heltal ")) # Andra heltalet från användaren. 
+summan = tal1 + tal2 # Adderar talen.
+print(summan)
 
-#Jacka = 2000 # Jackans pris.
-#Rea = 1000 # Rean på jackan. 
-#Betala = Jacka - Rea # Vad man ska betala.
-#print(Betala,"kr.") 
+jacka = 2000 # Jackans pris.
+rea = 1000 # Rean på jackan. 
+betala = jacka - rea # Vad man ska betala.
+print(betala,"kr.") 
 
-#Pris = 2000 # Jackans pris.
-#Rea_procent = 50 # Rean på jackan.
-#slut_pris = Pris * Rea_procent / 100 # Vad man ska betala.
-#print(slut_pris) 
+pris = 2000 # Jackans pris.
+rea_procent = 50 # Rean på jackan.
+slut_pris = pris * rea_procent / 100 # Vad man ska betala.
+print(slut_pris) 
 
-Jacka = 2000
-Procent = int(input("Skriv en procentsats"))
+jacka = 2000
+procent = int(input("Skriv en procentsats"))
+slutpris = jacka * procent / 100
+print(f"{slutpris:.0f}")
