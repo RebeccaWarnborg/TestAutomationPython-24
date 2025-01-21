68b644239090a5fbbb8a597e067d486bab18b008
RebeccaWarnborg <rebecca.warnborg@hotmail.com>
2025-01-21 13:12:00 +0100

    Sparat ändringar

--- Veckouppgift3/Veckouppgift3.3.py
+++ Veckouppgift3/Veckouppgift3.3.py
@@ -1,11 +1,20 @@
 movies = ["The nun", "The conjuring", "Anabelle", "The Exorcism"]
+
+# Lägg till "Fellowship of the ring" sist i listan.
 movies.append("Fellowship of the ring")
+
+# Lägg till "The two towers" på första platsen i listan
 movies.insert(0,"The two towers")
+
+# Ta bort en annan av filmerna. Har Fellowship-filmen ändrat index?
 movies.remove("The nun")
 print("Skräckfilmer: ")
 for movie in movies:
     print("- ", movie)
 
+#  Ta reda på vilken position (index) "Fellowship of the ring" har
 print("Filmen Fellowship of the ring har plats:",movies.index("Fellowship of the ring"))
+
+# Ta reda på hur lång listan är
 
 
