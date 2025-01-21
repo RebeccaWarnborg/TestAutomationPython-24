37eb6afdaeacfca7d860bda2ae480a8c52640245
RebeccaWarnborg <rebecca.warnborg@hotmail.com>
2025-01-21 11:10:22 +0100

    Sparat ändringar

--- Veckouppgift3/Veckouppgift3.2.py
+++ Veckouppgift3/Veckouppgift3.2.py
@@ -1,7 +1,7 @@
-#answer = 0
-#for i in range(1,101):
- #   answer += i
-#print("Summan av talen 1 till 10 är: " + str(answer))
+answer = 0
+for i in range(1,101):
+    answer += i
+print("Summan av talen 1 till 10 är: " + str(answer))
 
 answer = 0
 i = 0
