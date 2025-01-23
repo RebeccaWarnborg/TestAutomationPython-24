8d9f9bf94ed4c38735f661da34bc7d2a240f06b8
RebeccaWarnborg <rebecca.warnborg@hotmail.com>
2025-01-23 12:16:15 +0100

    Lagt till ny kod

--- Veckouppgift3/Veckouppgift3.4.py
+++ Veckouppgift3/Veckouppgift3.4.py
@@ -1,5 +1,6 @@
 # Gör ett program som upprepade gånger ber användaren skriva in ett tal. 
 # När man skriver in ngen "quit" eller "avsluta" ska programmet ska det räkna ut summan av talen
+# Version 2: programmet ska fråga hur många man är, och tala om hur mycket varje person i sällskapet ska betala.
 
 summa = 0
 
@@ -17,5 +18,11 @@ while True:
     except ValueError:
         print("Det var inte ett giltigt tal, försök igen.")
 
-# Här skrivs totalsumman ut när loopen avslutas
-print(f"Det blir {summa} kr totalt. Välkommen åter!")
+# Fråga användaren hur många personer de är
+antal_personer = int(input("Hur många är ni? "))
+
+# Räkna ut hur mycket varje person ska betala
+belopp_per_person = summa / antal_personer
+
+# Skriv ut totalsumman och vad varje person ska betala
+print(f"Det blir {summa} kr totalt, alltså {belopp_per_person:.2f} kr per person. Välkommen åter!")
