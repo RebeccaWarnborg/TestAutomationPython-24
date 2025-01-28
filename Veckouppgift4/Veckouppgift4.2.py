13778c3eff89e55c8923c3071ecd79a8ea199cd4
RebeccaWarnborg <rebecca.warnborg@hotmail.com>
2025-01-28 14:31:55 +0100

    Ny kod och funktioner

---
+++ Veckouppgift4/Veckouppgift4.2.py
@@ -0,0 +1,10 @@
+# 1 Skriv en funktion som tar en sträng som parameter.
+# När den anropas ska den skriva ut strängen och "är en fena på programmering". 
+# Exempel: my_function("David") → "David är en riktig hacker"
+
+from funktioner import my_function
+
+my_function("David")  # Utskrift: David är en fena på programmering
+
+my_function("Emma")  # Utskrift: Emma är en fena på programmering
+my_function("Lisa")   # Utskrift: Lisa är en fena på programmering
---
+++ Veckouppgift4/funktioner.py
@@ -0,0 +1,4 @@
+# Funktioner till veckouppgifter vecka 4. 
+
+def my_function(namn):
+    print(f"{namn} är en Fena på programmering")
