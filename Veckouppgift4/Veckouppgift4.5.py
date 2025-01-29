471b1f78e3306d8b83051208bf67dafc97a2bd31
RebeccaWarnborg <rebecca.warnborg@hotmail.com>
2025-01-29 15:33:28 +0100

    Redigerat kod

---
+++ Veckouppgift4/Veckouppgift4.5.py
@@ -0,0 +1,8 @@
+# Lös felen i koden.
+
+def increase(x):
+    x += 1
+    return x
+    
+
+print(increase(1))
--- Veckouppgift4/funktioner.py
+++ Veckouppgift4/funktioner.py
@@ -4,6 +4,5 @@ def my_function(namn):
     print(f"{namn} är en fena på programmering")
 
 def eko(text, count):
-   # resultat = text * 2  # Upprepa strängen två gånger
     print(text * count)  
     
