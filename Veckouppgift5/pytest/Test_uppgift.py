723834216586b5531a1bff2c670815f35334df0b
RebeccaWarnborg <rebecca.warnborg@hotmail.com>
2025-02-04 13:44:32 +0100

    Tester av kod

---
+++ Veckouppgift5/pytest/Test_uppgift.py
@@ -0,0 +1,17 @@
+# Det har smugit sig in kommentarer i stället för kod på några ställen. 
+# Skriv färdigt testfallen test_empty_list och test_number_list.
+
+# Returnerar summan av alla tal i listan
+def sum_list(list):
+    return sum(list)
+
+def test_empty_list():
+    expected = 0
+    actual = sum_list([])
+    assert actual == expected
+    
+def test_number_list():
+    assert sum_list([5]) == 5
+    expected = 5
+    actual = sum_list()
+    assert actual == expected 
