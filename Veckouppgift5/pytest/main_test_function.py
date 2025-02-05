6e9b7b1af5ea4fe101781318e5f61043a9b77d0c
RebeccaWarnborg <rebecca.warnborg@hotmail.com>
2025-02-05 18:50:37 +0100

    Ändrat kod

--- Veckouppgift5/pytest/main_test_function.py
+++ Veckouppgift5/pytest/main_test_function.py
@@ -3,3 +3,11 @@ def c_to_f(degree):
     if degree < -273.15:
         return None
     return degree * 9 / 5 + 32
+
+# Funktion som tar en sträng och returnerar antal ord från den.
+def count_words(sentence):
+    if sentence == sentence.isaplha():
+        return count_words
+    else :
+        return None
+    
---
+++ Veckouppgift5/pytest/test_count_words.py
@@ -0,0 +1,24 @@
+# Betrakta funktionen count_words(sentence), som tar en sträng och returnerar antalet ord. 
+# Ett ord är en serie tecken som separeras av mellanslag. 
+# Formulera de krav och acceptanskriterier (AK) som ska gälla för funktionen.
+
+# Task: Testa kod som ska returnera antalet ord från en sträng.
+# Acceptanskritierier:
+# Koden ska kunna returnera stora och små bokstäver.
+# Om annat än bokstäver skrivs så ska resultatet bli None.
+# Koden ska ignorera tecken och siffror i en sträng med text.
+# Resultatet ska bli None om ordet är blandat med siffor. 
+
+from main_test_function import count_words
+
+
+def test_count_words():
+    test_cases = [
+        ("HELLO WORLD", "test"),
+        ("hello world", "test"),
+        ("Hello World!", "test"),
+        ("H3ll0 w0rld", "test") ,
+    ]
+    for count_words in test_cases:
+        actual = count_words(test_cases)
+        assert actual == expected
