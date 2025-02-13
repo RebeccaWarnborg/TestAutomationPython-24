8be93e8ef9ad4ce0d7b7ddfc0718bb90a8d580d2
RebeccaWarnborg <rebecca.warnborg@hotmail.com>
2025-02-13 12:27:24 +0100

    Färdig med kod

--- Veckouppgift6/country.py
+++ Veckouppgift6/country.py
@@ -1,19 +1,14 @@
 # På Island bor det 383726 invånare och i Danmark 5961249. Skapa objekt för länderna. 
 # (Data från januari 2024. Avrunda befolkningen.)
 
-# Kod ej klar.
-
 class Country:
     def __init__(self, name, pop, area = None):
         self.__name = name
         self.__population = pop
         self.__area = area
-
-    def __str__(self):
-        return self.print_info()
     
     def print_info(self):
-        return f"I {self.get_name()} bor det {self.get_population()} miljoner invånare"
+        print(f"I {self.get_name()} bor det {self.get_population()} miljoner invånare")
 
     def get_name(self):
         return self.__name
@@ -24,6 +19,8 @@ class Country:
     def get_area(self):
         return self.__area
 
-
 se = Country("Sverige", 10.5)
 no = Country("Norge", 5.5)
+
+se.print_info()
+no.print_info()
