c16b507d48803ee620921242aea980238ae67a11
RebeccaWarnborg <rebecca.warnborg@hotmail.com>
2025-03-12 11:18:35 +0100

    Userstory and codeexample

---
+++ Testautomatisering_och_testverktyg/Veckouppgift2/userstory.py
@@ -0,0 +1,15 @@
+""" 
+User story för Agile helper sprint retrospective:
+Som användare vill jag via hemsidan https://lejonmanen.github.io/agile-helper/ kunna läsa hur jag gör en sprint retrospective
+så jag och teamet kan utvädera och avsluta sprinten.
+    
+Testscenario:
+* Navigera till hemsidan och trycka på följande knappar:
+Last --> End the sprint by evaluating your work in sprint retrospective.
+* Textruta med titel "Sprint retrospective" ska komma upp med information och knappen "The sprint is complete".
+* Tryck på knappen "The sprint is complete" och du hamnar på startsidan för Agile helper.
+
+Implicita tester: Att användargränssnittet funkar.
+Explicita tester: Att hemsidan navigerarar vidare och knappar funkar.
+
+"""
