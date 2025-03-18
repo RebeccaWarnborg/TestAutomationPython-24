0ae3083a6d02b8a5e6da03b7ed2320e16c1f470a
RebeccaWarnborg <rebecca.warnborg@hotmail.com>
2025-03-18 10:22:18 +0100

    User story

---
+++ Testautomatisering_och_testverktyg/Veckouppgift3/TimeApp1.py
@@ -0,0 +1,26 @@
+"""
+Test Time App 
+
+User stories:
+[U1] Som en användare vill jag kunna skapa och ta bort widgets för funktionerna timer och anteckning,
+så jag kan redigera efter önskemål och behov.
+
+Acceptanskriterier:
+[A1.1] När jag trycker på widget "Add Timer" ska en timer visas på skärmen.
+[A1.2] När jag trycker på ikonen som simulerar en papperskord ska min timer försvinna.
+[A2.1] När jag trycker på widget "Add Note" ska en tom ruta visas på skärmen som jag kan skriva text i.
+[A2.2] När jag trycker på ikonen som simulerar en papperskord ska min note försvinna.
+
+
+Testscenarier:
+[T1]
+1. Kontrollera att knappen "Add timer" finns på skärmen.
+2. Kontrollera att en timer visas på skärmen när man trycker på knappen "Add timer".
+3. Tryck på knappen som simulerar en papperskorg och kontrollera att timer'n försvinner.
+
+[T2]
+1. Kontrollera att knappen "Add note" finns på skärmen.
+2. Kontrollera att en tom ruta för textinmatning visas på skärmen när man trycker på knappen "Add note".
+3. Tryck på knappen som simulerar en papperskorg och kontrollera att rutan anteckningen försvinner.
+
+"""
