26b4def9e13fc62336508ec7aa6360a70576c40d
RebeccaWarnborg <rebecca.warnborg@hotmail.com>
2025-03-18 11:21:10 +0100

    Added user story's

---
+++ Testautomatisering_och_testverktyg/Veckouppgift3/TimeApp2.py
@@ -0,0 +1,19 @@
+"""
+Test Time App
+
+User stories:
+[U1] Som användare vill jag kunna byta plats på mina widgets så jag kan strukturera det efter mina önskemål.
+
+Acceptanskriterier:
+[A1] När det finns minst två widgets så ska jag kunna byta plats på dessa.
+
+Testscenarier:
+[T1]
+1. Kontrollera att knappen "Add timer" finns på skärmen.
+2. Kontrollera att knappen "Add note" finns på skärmen.
+3. Tryck på knapparna "Add timer" och "Add note".
+4. Kontrollera att en ikon som simulerar en pil visas på skärmen.
+5. Tryck på ikonen som simulerar en pil.
+6. Kontrollera att den widget vars pil du tryckte på bytte plats ett steg upp.
+
+"""
---
+++ Testautomatisering_och_testverktyg/Veckouppgift3/TimeApp3.py
@@ -0,0 +1,25 @@
+"""
+Test Time App
+
+User stories:"
+Som en användare vill jag kunna ändra tidsinställningarna på timer'n så den passar mina önskemål.
+
+Acceptanskriterier:
+[A1] Redigera tid för timer'n.
+
+Testscenarier:
+[T1] 
+1. Kontrollera att knappen "Add timer" finns på skärmen.
+2. Tryck på knapparna "Add timer".
+3. Kontrollera att en timer finns på skärmen.
+4. Kontrollera att en ikon som simulerar ett kugghjul finns på skärmen.
+5. Tryck på ikonen som simulerar ett kugghjul.
+6. Kontrollera att en ruta med alternativ för att ändra tid på timer'n syns.
+7. Kontrollera att ikonen "Counting down" syns.
+8. Ändra tiden i rutan för timer'n.
+9. Tryck på switch'en för "Counting down".
+10. Kontrollerna att tiden på timer'n har ändrats till den nya tiden som angavs.
+11. Tryck på "Start".
+12. Kontrollera att timer'n räknar ner från den nya tiden som angavs.
+
+"""
